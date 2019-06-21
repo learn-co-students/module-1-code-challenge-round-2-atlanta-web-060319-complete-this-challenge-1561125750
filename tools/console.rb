dccc98c32753a409b84ff63b5d79f42454a7069e
github username <github email address>
2019-06-21 11:18:19 -0400

    soo happy this is over

--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+reina = Viewer.new("rere")
+austin = Viewer.new("babyface")
+ellen = Viewer.new("ellls") 
+
+this_is_40 = Movie.new("This is 40")
+sleepy_hollow = Movie.new("Sleep Hollow")
+nemo = Movie.new("Nemo")
+
+one = QueueItem.new(reina, nemo, 5)
+two = QueueItem.new(austin, this_is_40, 3)
+three = QueueItem.new(ellen, sleepy_hollow, 2)
+four = QueueItem.new(reina, this_is_40, 8)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
