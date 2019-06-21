02c1222801523a6386433e1c9b072973e75e1e6a
Aeisha Hicks <ashicks2017@outlook.com>
2019-06-21 11:12:02 -0400

    completed relationships for code

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,16 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select do |queue|
+      queue.viewer == self
+    end
+  end
+
+  def queue_movies
+  queue_items.map do |queue|
+    queue.movie
+   end
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ space = Movie.new("Space James")
+ pretty = Movie.new("pretty")
+ men_in_black = Movie.new("Men In Black")
+
+ ace = Viewer.new("Ace")
+ mike = Viewer.new("Mike")
+ tank = Viewer.new("Tank")
+
+ que1 = QueueItem.new(space, ace, 5)
+ que2 = QueueItem.new(pretty, mike, 3)
+ que3 = QueueItem.new(men_in_black, tank, 1)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
