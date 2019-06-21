f15cd54e4d7251b7c6cb9813008ff1278bd77572
github username <github email address>
2019-06-21 11:18:57 -0400

    Crushed it

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,27 @@ class Movie
     @@all
   end
 
+  #helper method
+  def queue_items
+    QueueItem.all.select{ |item| item.movie == self}
+  end
+
+  def viewers
+      queue_items.map{|item| item.viewer}
+  end
+
+
+  def average_rating
+    rating = 0
+    all_queue = queue_items.map do |queue|
+      rating  += queue.rating
+  end
+    rating = rating.to_f/ all_queue.length
+  end
+
+  def highest_rated
+      queue_items.sort_by{|queue| queue.rating}.last
+  end
+
+
 end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,19 @@
 class QueueItem
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
+  
+    def initialize(viewer, movie, rating=0)
+      @viewer = viewer
+      @movie = movie
+      @rating = rating
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+
 
 
 end
