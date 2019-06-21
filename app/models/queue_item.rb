031a7dedfdb936da718315d77493083f0439be8b
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-21 11:24:55 -0400

    crushed it

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,22 @@
 class QueueItem
 
+    attr_accessor :movie, :viewer, :rating
+
+    @@queueitems = []
+
+    def initialize(movie, viewer, rating = nil)
+        @movie = movie 
+        @viewer = viewer
+        @rating = rating
+        @@queueitems << self 
+    end 
+
+    def self.all
+        @@queueitems
+    end 
+
+    
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+inception = Movie.new("inception")
+lionking = Movie.new("lionking")
+coolrunnings = Movie.new("coolrunnings")
+aladdin = Movie.new("aladdin")
+
+
+jourdan = Viewer.new("jourdan")
+jacque = Viewer.new("jacque")
+claudette = Viewer.new("claudette")
+
+
+queueitem1 = QueueItem.new(inception, jourdan, 9)
+queueitem2 = QueueItem.new(lionking, jacque, 7)
+queueitem3 = QueueItem.new(coolrunnings, claudette, 5)
+queueitem4 = QueueItem.new(lionking, jourdan, 10)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
